99160fac3869b39a4ef074b482f190ea9499c3c5
Ashley <139553561+AshTheDergy@users.noreply.github.com>
2023-08-28 22:03:34 +0300

    Update boydancer.js

--- Commands/Boydancer/boydancer.js
+++ Commands/Boydancer/boydancer.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const { spawn, execSync } = require('child_process');
+const { spawn } = require('child_process').spawn;
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
 const { CommandInteraction } = require("discord.js");
 const PH = require("../../handlers/Client");
 const cooldowns = new Map();
@@ -8,11 +9,9 @@ const humanizeDuration = require('humanize-duration');
 
 module.exports = {
     name: "boydancer",
-    description: `apply audio (currently only audio files) to boykisser dancing video max 60 seconds!! (in beta)`,
+    description: `Apply audio (currently only audio files) to boykisser dancing video max 60 seconds!! (in beta)`,
     userPermissions: ['SEND_MESSAGES'],
     botPermissions: ["EMBED_LINKS"],
-    category: "Video",
-    //cooldown: 15,
     type: "CHAT_INPUT",
     options: [
         {
@@ -27,34 +26,29 @@ module.exports = {
         },
         {
             name: "start",
-            description: "(0:00) Leaving this blank will cause it to use the audio from 0:00",
+            description: "(format 0:00) Leaving this blank will cause it to use the audio from 0:00",
             type: 3,
         },
         {
             name: "end",
-            description: "(0:00) Leaving this blank will make the video 1 minute long",
+            description: "(format 0:00) Leaving this blank will make the video 1 minute long",
             type: 3,
         }
     ],
 
     /**
-    *
     * @param {PH} client
     * @param {CommandInteraction} interaction
-    * @param {String[]} args
     */
-    run: async (client, interaction, args) => {
+    run: async (client, interaction) => {
 
         const author = interaction.user.id;
         const cooldown = cooldowns.get(author);
-
         cooldownUser(author, 1);
 
         const emojiSuccess = client.config.emoji.success;
         const emojiError = client.config.emoji.error;
 
-        const correctAudio = [".mp3", ".wav", ".aac", ".flac", ".ogg"];
-        const correctVideo = [".mp4", ".avi", ".mov", ".webm", ".3gp", ".mkv", ".qt"];
         const correctFile = [".mp3", ".wav", ".aac", ".flac", ".ogg", ".mp4", ".avi", ".mov", ".webm", ".3gp", ".mkv", ".qt"]
         const correctYoutube = "https://www.youtu";
         const correctShareYoutube = "https://youtu.be"
@@ -68,16 +62,17 @@ module.exports = {
         var danceEnd = 60
 
         const randomFileName = interaction.user.id;
+        const ffmpegPath = `./Commands/Boydancer/ffmpeg.exe`
         const boyDancerFile = `./files/permanentFiles/theboydancer.mp4`;
         const outputVideoPath = `./files/temporaryFinalVideo/${randomFileName}.mp4`;
         const tempYoutubePath = `./files/temporaryYoutube/${randomFileName}.wav`;
 
-    if (cooldown && !interaction.user.id == "817843037593403402") {
+    if (cooldown) {
         const remaining = humanizeDuration(cooldown - Date.now(), { units: ['m', 's'], round: true });
         client.embed(
             interaction,
             `You are On Cooldown, wait \`${remaining}\``
-        )
+        );
     } else if (!audioUrl && !audioFile) {
         client.embed(
             interaction,
@@ -106,7 +101,7 @@ module.exports = {
             cooldownUser(author, 10);
             return;
         } else if (a.startsWith(correctYoutube) || a.startsWith(correctShareYoutube)) {
-            const videoId = await extractVideoId(audioUrl);
+            /*const videoId = await extractVideoId(audioUrl);
             if (!videoId) {
                 client.embed(
                     interaction,
@@ -114,7 +109,7 @@ module.exports = {
                 );
                 cooldownUser(author, 10);
                 return;
-            }
+            }*/
             const videoLive = await checkLiveStatus(audioUrl);
             if (videoLive) {
                 client.embed(
@@ -128,8 +123,8 @@ module.exports = {
                 if (length > 600) {
                     client.embed(
                         interaction,
-                        `${emojiError} - ** Please make sure the __Youtube Video__ is __10 minutes (600 seconds)__ or less long **`
-                    )
+                        `${emojiError} - ** Please ensure that the __YouTube Video__ is __10 minutes (600 seconds)__ or shorter **`
+                    );
                 } else {
                     await downloadYoutubeVideo(audioUrl);
                         if (startTime && endTime) {
@@ -220,6 +215,7 @@ module.exports = {
                                 fs.unlinkSync(outputVideoPath);
                                 fs.unlinkSync(tempYoutubePath);
                                 cooldownUser(author, 60);
+                                await client.usage.set(`${interaction.user.id}.successful`, usedSuccessful ? parseInt(usedSuccessful) + 1 : 1);
                             } catch (error) {
                                 console.error('Error generating the video:', error);
                                 interaction.followUp('An error occurred while generating the video.');
@@ -312,17 +308,18 @@ module.exports = {
                 };
                 cooldownUser(author, 1);
                 try {
-                    await applyAudioToVideoFILE(af, danceStart, length < 60 ? length : danceEnd);
+                    await applyAudioToVideoFILE(audioUrl, danceStart, length < 60 ? length : danceEnd);
                     await interaction.followUp({content: `${emojiSuccess} - Here is your boydancer:`, files: [{ attachment: outputVideoPath, name: "Boydancer.mp4"}]});
                     fs.unlinkSync(outputVideoPath);
                     cooldownUser(author, 60);
+                    await client.usage.set(`${interaction.user.id}.successful`, usedSuccessful ? parseInt(usedSuccessful) + 1 : 1);
                 } catch (error) {
                     console.error('Error generating the video:', error);
                     interaction.followUp('An error occurred while generating the video.');
                 }
             } else {
                 interaction.followUp('An error occurred while getting file data.');
-                interaction.followUp('if you did everything correctly and think it\'s a bug, make a report \`/report boydancer\`', {ephemeral: true});
+                interaction.followUp('if you did everything correctly and think it\'s a bug, make a report \`/report bug\`', {ephemeral: true});
                 cooldownUser(author, 10);
             }
         }
@@ -425,6 +422,7 @@ module.exports = {
                     await interaction.followUp({content: `${emojiSuccess} - Here is your boydancer:`, files: [{ attachment: outputVideoPath, name: "Boydancer.mp4"}]});
                     fs.unlinkSync(outputVideoPath);
                     cooldownUser(author, 60);
+                    await client.usage.set(`${interaction.user.id}.successful`, usedSuccessful ? parseInt(usedSuccessful) + 1 : 1);
                 } catch (error) {
                     console.error('Error generating the video:', error);
                     interaction.followUp('An error occurred while generating the video.');
@@ -438,7 +436,7 @@ module.exports = {
         let replies = [`Ha Ha very funny. "lem e putt url an fil as jok :nerd::nerd::nerd:`, `No...`, `Kindly deactive yourself :blush:`, `<:oppy:1144574623736922283>`, `Moderators, crush his skull`, `I'm gonna fuck your mother`, `2 Inputs, 2 Braincells`, `Go **Link** yourself some bitches and make them **Audio**`, `hey buddy. we're going to kill you and leave you laid out in a dumpster to rot`, `https://cdn.discordapp.com/attachments/873603423998705718/1145258850132443206/8apAlKE.gif`, `https://cdn.discordapp.com/attachments/873603423998705718/1145258963676430346/cqtykgb.gif`];
         let randomMessage = Math.floor(Math.random() * replies.length);
         interaction.followUp(replies[randomMessage]);
-        cooldownUser(author, 300);
+        cooldownUser(author, 60);
         return;
     }
 
@@ -507,75 +505,47 @@ module.exports = {
     };
 
     async function applyAudioToVideoFILE(file, start, end) {
-        const duration = /*Math.floor(*/end - start//)
-        const ffmpegArgs = [
-            '-ss', "0",
-            '-i', boyDancerFile,
-            '-ss', start.toString(),
-            '-i', file,
-            '-filter_complex', '[0:a]volume=0.2[audio];[1:a]volume=1[music];[audio][music]amix=inputs=2:duration=first:dropout_transition=2[audioout]',
-            '-map', '0:v',
-            '-map', '[audioout]',
-            '-c:v', 'copy',
-            '-c:a', 'aac',
-            '-t', duration.toString(),
-            '-y',
-            outputVideoPath,
-        ];
-
-        const ffmpegProcess = spawn(client.config.ffmpegPath, ffmpegArgs);
-
-        ffmpegProcess.stderr.on('data', (data) => {
-            console.error(`FFmpeg stderr: ${data}`);
-          });
-
-        ffmpegProcess.on('close', (code) => {
-            if (code !== 0) {
-              const errorMessage = `FFmpeg process exited with code ${code}`;
-              throw new Error(errorMessage);
-            }
-          });
-        
-          return new Promise((resolve, reject) => {
-            ffmpegProcess.on('exit', (code) => {
-              if (code === 0) {
+    const duration = end - start;
+    return new Promise((resolve, reject) => {
+        const ffmpegProcess = ffmpeg()
+            .input(`./files/permanentFiles/theboydancer.mp4`)
+            .inputOptions(['-ss 0', '-ss ' + start.toString()])
+            .input(file)
+            .complexFilter([
+                '[0:a]volume=0.2[audio];[1:a]volume=1[music];[audio][music]amix=inputs=2:duration=first:dropout_transition=2[audioout]'
+            ])
+            .outputOptions(['-map 0:v', '-map [audioout]', '-c:v copy', '-c:a aac', '-t ' + duration.toString(), '-y'])
+            .output(outputVideoPath)
+            .on('error', (err) => {
+                reject(err);
+            })
+            .on('end', () => {
                 resolve(outputVideoPath);
-              } else {
-                reject(new Error(`FFmpeg process exited with code ${code}`));
-              }
             });
-          });
-    };
 
-    //functions for files (i didnt write half of them :<)
+        ffmpegProcess.run();
+    });
+}
 
     async function getVideoDuration(videoUrl) {
-        const command = `${client.config.ffmpegPath} -i "${videoUrl}" 2>&1 | find "Duration"`;
-    
-        try {
-            const stdout = execSync(command).toString();
-            const durationLine = stdout.trim();
-            const durationMatch = /Duration: (\d{2}:\d{2}:\d{2}\.\d{2})/.exec(durationLine);
-    
-            if (durationMatch) {
-                const durationParts = durationMatch[1].split(':');
-                const hoursInSeconds = parseInt(durationParts[0]) * 3600;
-                const minutesInSeconds = parseInt(durationParts[1]) * 60;
-                const seconds = parseFloat(durationParts[2]);
-                const totalDurationInSeconds = hoursInSeconds + minutesInSeconds + seconds;
-    
-                return totalDurationInSeconds;
-            } else {
-                return null;
-                throw new Error('Could not determine video duration.');
+    return new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(videoUrl, (err, metadata) => {
+            if (err) {
+                reject(err);
+                return;
             }
-        } catch (error) {
-            console.error(error);
-        }
-    }
+            if (metadata && metadata.format && metadata.format.duration) {
+                const totalDurationInSeconds = parseFloat(metadata.format.duration);
+                resolve(totalDurationInSeconds.toFixed(1));
+            } else {
+                resolve(null);
+            }
+        });
+    });
+}
 
    },
-}
+};
 /*
 ⣿⣿⡟⡹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⢱⣶⣭⡻⢿⠿⣛⣛⣛⠸⣮⡻⣿⣿⡿⢛⣭⣶⣆⢿⣿

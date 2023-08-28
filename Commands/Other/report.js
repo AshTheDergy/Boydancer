c8276942a9146eb27e8a3bc354dca6b279ecf288
Ashley <139553561+AshTheDergy@users.noreply.github.com>
2023-08-28 22:10:35 +0300

    Update and rename ping.js to report.js

---
+++ Commands/Other/report.js
@@ -0,0 +1,55 @@
+const { CommandInteraction } = require("discord.js");
+const PH = require("../../handlers/Client");
+const cooldowns = new Map();
+const humanizeDuration = require('humanize-duration');
+
+module.exports = {
+    name: "report",
+    description: `Report bugs related to Boydancer`,
+    userPermissions: ["SEND_MESSAGES"],
+    botPermissions: ["EMBED_LINKS"],
+    category: "Information",
+    type: "CHAT_INPUT",
+    options: [
+        {
+            name: "bug",
+            description: "Report bugs related to Boydancer",
+            type: 1,
+            options: [
+                {
+                    name: "issue",
+                    description: "Describe the issue",
+                    type: 3,
+                    required: true,
+                },
+            ],
+        },
+    ],
+
+    /** 
+    * @param {PH} client
+    * @param {CommandInteraction} interaction
+    */
+    run: async (client, interaction) => {
+        const author = interaction.user.id;
+        const cooldown = cooldowns.get(author);
+        if (cooldown) {
+            const remaining = humanizeDuration(cooldown - Date.now(), { units: ['m', 's'], round: true });
+            client.embed(
+                interaction,
+                `You are On Cooldown, wait \`${remaining}\``
+            );
+        } else {
+            const issue = interaction.options.getString("issue");
+            const size = await client.bugs.size;
+            await client.bugs.set(`${size}.bugs`, `${interaction.user.username} (${interaction.user.id}) - ${issue}`);
+            await interaction.followUp(`Bug report has been sent! Thank you!`);
+            cooldownUser(author, 1800);
+        }
+
+        function cooldownUser(user, time) {
+            cooldowns.set(user, Date.now() + time * 1000);
+            setTimeout(() => cooldowns.delete(interaction.user.id), time * 1000);
+        }
+    },
+};

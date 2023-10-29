78d1d966231bae7aa4434620cd4096da01fd4e15
Ashley <139553561+AshTheDergy@users.noreply.github.com>
2023-10-29 19:59:12 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 module.exports = {
-    TOKEN: process.env.TOKEN, //replace with your token
+    news: {
+    	version: '1.2.3',
+        new: [
+            '- something cool',
+            '- something fun',
+        ],
+	},
+    TOKEN: process.env.TOKEN,
     embed: {
         color: "#220f80",
         wrongcolor: "#8a0808",
-        footertext: "Made by AshTheDergy | Fluffy Derg Productions", //you can change this
+        footertext: "Made by AshTheDergy | Fluffy Derg Productions"
+    },
+    slash: {
+        global: true,
+        guildID: "924707637659062373",
     },
     options: {
         embedFooter: true,
@@ -14,4 +25,4 @@ module.exports = {
         success: "✅",
         ping: "🏓",
     },
-}
+};

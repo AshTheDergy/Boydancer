adc5bd6fa1f8c3c55713c5f48d3cb257a56641d0
Ashley <139553561+AshTheDergy@users.noreply.github.com>
2023-11-25 22:21:19 +0200

    Remade some things in commonfunctions
    Filter systems

--- functions/CommonFunctions.js
+++ functions/CommonFunctions.js
@@ -56,19 +56,13 @@ function giveSecondsFromTime(author, input) {
 
 async function applyAudioToVideoFILE(interaction, file, start, end, danceEnd) {
 
-    // modifiers
-
-    const modifier = interaction.options.getInteger("modifiers");
-
     // Speed
-
     const selectedSpeed = interaction.options.getInteger("speed");
     const beatsPerMin = interaction.options.getInteger("bpm");
     const audioSpeed = selectedSpeed || 100;
     const normalizedAudioSpeed = Math.min(200, Math.max(50, audioSpeed));
 
     // Volume
-
     const audioVolume = interaction.options.getInteger("volume");
     const volume = !audioVolume ? 100 : audioVolume > 500 ? 500 : audioVolume < 10 ? 10 : audioVolume;
 
@@ -83,47 +77,51 @@ async function applyAudioToVideoFILE(interaction, file, start, end, danceEnd) {
     // Paths
     const outputVideoPath = `./files/temporaryFinalVideo/${interaction.user.id}.mp4`;
 
-    // Size
-
-    const sizeModifier = 0.94 // reduce this to make the videos lower (do not go above 0.99)
-    let maxMB = getMaxMB(interaction.guild.premiumTier);
-    const reducerNum = Math.max(Math.round((modifier == 2 ? 25 : maxMB / (duration - duration * sizeModifier)) * 10) / 10, 0.1);
-    const reducer = reducerNum > 1 ? 1 : reducerNum;
-
     // BPM
-
     const bpm = !beatsPerMin ? getViberBPM(viber) : beatsPerMin > 500 ? 500 : beatsPerMin < 10 ? 10 : beatsPerMin;
     const beat = getViberBPM(viber);
 
+    // Filter
+    const modifier = interaction.options.getInteger("modifiers");
+
+    // Size
+    const sizeModifier = 0.94 // reduce this to make the videos lower (do not go above 0.99)
+    let maxMB = getMaxMB(interaction.guild.premiumTier);
+    const reducerNum = Math.max(Math.round((modifier == 2 ? 25 : maxMB / (duration - duration * sizeModifier)) * 10) / 10, 0.1); //2x2
+    const reducer = reducerNum > 1 ? 1 : reducerNum;
+
+    // Filter modification
+    
+    let modifiers = {
+        cfilter: `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100}[music];[music]amix=inputs=1[audioout]`,
+        vbit: 0,
+        abit: 0,
+        vfilter: `setpts=${beat / bpm}*PTS,scale=iw*${reducer > 1 ? 1 : reducer}:-1`,
+    };
+
+    if (modifier) {
+        await chooseFilter(modifier, modifiers, normalizedAudioSpeed, volume, beat, bpm);
+    };
+
     return new Promise((resolve, reject) => {
         const ffmpegProcess = ffmpeg()
-            .input(backgroundViber)
-            .inputOptions(['-ss 0', '-stream_loop -1'])
-            .input(file)
-            .inputOptions(['-ss ' + start.toString()])
-            .complexFilter([
-                modifier == 1 ? `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 67}[music];[music]amix=inputs=1[audioout]` :
-                modifier == 3 ? `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aecho=0.8:1:10:1[reverb];[reverb]amix=inputs=1[audioout]` :
-                modifier == 4 ? `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aecho=0.6:1:400:0.8[reverb];[reverb]amix=inputs=1[audioout]` :
-                modifier == 5 ? `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aformat=sample_fmts=s16:channel_layouts=stereo,lowpass=300[underwater];[underwater]amix=inputs=1[audioout]` :
-                `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100}[music];[music]amix=inputs=1[audioout]`,
-            ])
-            .videoBitrate(modifier == 1 ? '10k' : modifier == 6 ? '1k' : 0)
-            .audioBitrate(modifier == 1 || modifier == 6 ? '1k' : 0)
-            .outputOptions([
-                '-map 0:v',
-                '-map [audioout]',
-                '-c:v libx264',
-                '-c:a aac',
-                '-t ' + duration.toString(),
-                '-y',
-            ])
-            .output(outputVideoPath)
-            .videoFilter(
-                modifier == 1 ? `setpts=${beat / bpm}*PTS,scale=iw:-1` :
-                modifier == 6 ? `setpts=10*PTS,scale=2:2` :
-                `setpts=${beat / bpm}*PTS,scale=iw*${reducer > 1 ? 1 : reducer}:-1`
-            )
+        .input(backgroundViber)
+        .inputOptions(['-ss 0', '-stream_loop -1'])
+        .input(file)
+        .inputOptions(['-ss ' + start.toString()])
+        .complexFilter(modifiers.cfilter)
+        .videoBitrate(modifiers.vbit)
+        .audioBitrate(modifiers.abit)
+        .outputOptions([
+            '-map 0:v',
+            '-map [audioout]',
+            '-c:v libx264',
+            '-c:a aac',
+            '-t ' + duration.toString(),
+            '-y',
+        ])
+        .output(outputVideoPath)
+        .videoFilter(modifiers.vfilter)
             .on('error', (err) => {
                 reject(err);
             })
@@ -174,5 +172,37 @@ function getMaxMB(guild) {
     if (guild == 3) return 100;
     return 25
 }
+
+// Filters/Modifiers
+
+async function chooseFilter(filter, modifier, normalizedAudioSpeed, volume, beat, bpm) {
+
+    switch (filter) {
+
+        case config.Filters.troll:
+            modifier.cfilter = `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 67}[music];[music]amix=inputs=1[audioout]`;
+            modifier.abit = "1k";
+            modifier.vbit = "10k";
+            modifier.vfilter = `setpts=${beat / bpm}*PTS,scale=iw:-1`;
+
+        case config.Filters.mb25: //25mb
+       ;
+
+        case config.Filters.bathroom: //reverb (bathroom)
+            modifier.cfilter = `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aecho=0.8:1:10:1[reverb];[reverb]amix=inputs=1[audioout]`;
+
+        case config.Filters.emptyRoom: //reverb (empty room)
+            modifier.cfilter = `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aecho=0.6:1:400:0.8[reverb];[reverb]amix=inputs=1[audioout]`;
+
+        case config.Filters.underwater:
+            modifier.cfilter = `[1:a]atempo=${normalizedAudioSpeed / 100},volume=${volume / 100},aformat=sample_fmts=s16:channel_layouts=stereo,lowpass=300[underwater];[underwater]amix=inputs=1[audioout]`;
+
+        case config.Filters.twotwo: //2x2
+            modifier.abit = "1k";
+            modifier.vbit = "1k";
+            modifier.vfilter = `setpts=10*PTS,scale=2:2`;
+
+    } return modifier;
+};
 
 module.exports = { cooldownUser, giveSecondsFromTime, applyAudioWithDelay, getVideoDuration, getFinalFileName };
